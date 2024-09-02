3cda1521df538f90e8e929630b96510953e6ea2a
melvinwaluyo <melvinwaluyo@mail.ugm.ac.id>
2024-09-02 14:33:28 +0700

    added classes

---
+++ Demeter/Payment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demeter
+{
+    internal class Payment
+    {
+        public int PaymentID { get; set; }
+        public string Metode { get; set; }
+        public string Status { get; set; } // Pending or Selesai
+
+        public void ProsesPayment()
+        {
+            // Implementation of processing payment
+        }
+
+        public void UpdateStatusPayment(string status)
+        {
+            // Implementation of updating payment status
+        }
+    }
+}
---
+++ Demeter/Seller.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demeter
+{
+    internal class Seller : User
+    {
+        public string namaToko { get; set; }
+        public int noTelp { get; set; }
+        public string alamat {  get; set; }
+
+        public void addProduk(Produk produk)
+        {
+            produk.namaToko = this.namaToko;
+        }
+        public void deleteProduk(int produkId)
+        {
+            // TODO
+        }
+        public void updateProduk(Produk produk)
+        {
+
+        }
+    }
+}
