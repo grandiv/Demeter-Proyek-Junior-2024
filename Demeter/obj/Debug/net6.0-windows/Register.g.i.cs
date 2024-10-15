b2e849433eaaf1b3fdc63120634fa11af9575e6b
Grandiv <grandivfarand@gmail.com>
2024-10-15 21:20:56 +0700

    pull from remote 49342

--- Demeter/obj/Debug/net6.0-windows/Register.g.i.cs
+++ Demeter/obj/Debug/net6.0-windows/Register.g.i.cs
@@ -53,7 +53,7 @@ namespace Demeter {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Demeter;V1.0.0.0;component/register.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Demeter;component/register.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\Register.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
