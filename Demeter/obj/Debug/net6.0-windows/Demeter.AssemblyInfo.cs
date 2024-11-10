a52d1cbb5671c043e789f5eb154201f316f92085
Grandiv <grandivfarand@gmail.com>
2024-11-10 23:21:57 +0700

    feat: FE cart page

--- Demeter/obj/Debug/net6.0-windows/Demeter.AssemblyInfo.cs
+++ Demeter/obj/Debug/net6.0-windows/Demeter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Demeter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8f671061ad6b68df8c2d9ddd44f7100824729b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ff338dd6948e3a444e43b890cfcc20193e1cb59")]
 [assembly: System.Reflection.AssemblyProductAttribute("Demeter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Demeter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
